5eb000d2c0f17209908ca444fef1ba3b95e931a7
Emerson Marinho <Emerson_marinhoaz@hotmail.com>
2022-11-29 19:29:23 -0300

    exercises

--- fundamentos/secao-4-introducao-a-javascript/dia-3-javascript-logica-algoritmos/exercicios.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-3-javascript-logica-algoritmos/exercicios.js
@@ -16,3 +16,23 @@ let reverseWord = "";
 reverseWord = word.split("").reverse("").join("");
 
 console.log(reverseWord);
+
+let array = ["java", "javascript", "python", "html", "css"];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (arr of array) {
+  if (arr.length > biggestWord.length) {
+    biggestWord = arr;
+  }
+}
+
+for (arr of array) {
+  if (arr.length < smallestWord.length) {
+    smallestWord = arr;
+  }
+}
+
+console.log(biggestWord);
+console.log(smallestWord);
